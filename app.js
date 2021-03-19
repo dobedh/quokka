bcf40785f096f1aea9a574275da8cf85b1f90f56
dobedh <dobedh@gmail.com>
2021-03-19 23:54:29 +0900

    basic middlewares install

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 import express from "express"
+import helmet from "helmet"
+import morgan from "morgan"
+import bodyParser from "body-parser"
+import cookieParser from "cookie-parser"
+
 const app = express()
+
+app.use(helmet())
+app.use(morgan("dev"))
+app.use(bodyParser())
+app.use(cookieParser)
+
 
 const handleHome = (req,res) => res.send("Hello Quokka :)")
 
