801d42145e33b34a1e215e7e2c9cd997df46001f
dobedh <dobedh@gmail.com>
2021-03-20 21:59:28 +0900

    Router & Controller setting - 210320

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ import helmet from "helmet"
 import morgan from "morgan"
 import bodyParser from "body-parser"
 import cookieParser from "cookie-parser"
+import routes from "./routes"
+import globalRouter from "./router/globalRouter"
+import { localMiddleware } from "./localMiddleware"
 
 const app = express()
 
 app.use(helmet())
 app.use(morgan("dev"))
 app.use(bodyParser())
-app.use(cookieParser)
+app.use(cookieParser())
 
+app.use(localMiddleware)
 
-const handleHome = (req,res) => res.send("Hello Quokka :)")
-
-app.get("/", handleHome)
+app.use(routes.home, globalRouter)
 
 
 export default app
