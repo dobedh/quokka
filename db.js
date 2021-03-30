365100823e6194ff0a61190f1dca3aaba078f684
dobedh <dobedh@gmail.com>
2021-03-30 22:59:07 +0900

    grid layout for nav, content

--- db.js
+++ db.js
@@ -11,7 +11,7 @@ mongoose.connect(process.env.MONGO_URL, {
 
 const db = mongoose.connection;
 
-const handleOpen = () => console.log("✅ DB Connected!");
+const handleOpen = () => console.log("✅  DB Connected!");
 const handleError = (error) => console.log(`Error in DB : ${error}`);
 
 db.once("open", handleOpen);
